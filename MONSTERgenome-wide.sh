b789558cc479d858455434797d89df12f11c0d02
dsuveges <suvi.dani@gmail.com>
2017-05-10 17:00:40 +0100

    update

--- MONSTERgenome-wide.sh
+++ MONSTERgenome-wide.sh
@@ -7,25 +7,17 @@
 ## driven by a single variant or more by repeating the test with removing a variant
 ## each time.
 
-version="v10.3 Last modified: 2017.03.15"
+# For more information on how to use the wrapper, please see the README.md file or check the repository:
+# https://github.com/wtsi-team144/burden_testing
 
-# New in this version:
-    # The test if a single variant drives the association is now optional
-    # New scores are added: Linsight and mixed.
-    # Directory structure is more stringent
-    # Within a run, traits are separated in subfolders.
+# Todo for the next generation of the wrapper:
+## An older verision of MONSTER had an issue why we had to wrap genes individually for 
 
-##
-## todo list for the next release:
-##  # 1. We have to modify the test function.
-    #### Only one variant is tested the one with the lowest single point p-value.
-    #### The genotype of variant will be added as covariate to the phenotype file.
-    #### Re-run MONSTER once and report p-value.
-    ####
+version="v10.5 Last modified: 2017.05.09" # This version mainly changes in the documentation.
 
 today=$(date "+%Y.%m.%d") # Get the date
 
-# The variant selector script, that generates input for MONSTER:
+# The variant selector script, that generates snp and genotype input for MONSTER:
 regionSelector=burden_get_regions.pl
 
 # Folder with all the scripts:
@@ -37,8 +29,6 @@ phenotypeDir=/lustre/scratch115/projects/t144_helic_15x/analysis/HA/phenotypes/c
 
 # File with all the gene names (only on autosomes):
 geneListFile=${scriptDir}/gene_list.lst
-
-# MONSTER link that has to be adjusted:
 
 # Kinship matrix file:
 kinshipMatrix=/nfs/team144/ds26/burden_testing/kinship/2016.10.20_fix_diagonal/kinship.fixdiag.txt
@@ -52,11 +42,12 @@ MONSTER=/nfs/team144/software/MONSTER_v1.3/MONSTER
 # By default we are not keeping temporary files:
 keep_temp=0
 
+# By default the hit threshold is 1e-5
+export signifThreshold=1e-5
+
 # By default, the first chunk is read, or it can be submitted using -d or form jobindex:
 # By default we process all genes at once.
 export chunkCount=1 # How many chunks is the gene list split into?
-
-# Which chunk we are processing?
 export chunkNo=1 # Default value
 if [[ $LSB_JOBINDEX > 0 ]]; then chunkNo=$LSB_JOBINDEX; fi # If jobindex is available, we set that as chunk number.
 
@@ -83,6 +74,7 @@ It pools results together within one chunk."
     echo "     -s  - turn weights on. Arguments: GWAVA, CADD, Eigen, EigenPC, EigenPhred, EigenPCPhred, Linsight, Mixed"
     echo "     -x  - extend genomic regions (bp) (de)."
     echo "     -o  - exclude all non loss-of-function variants from the test."
+    echo "     -f  - include only HC and LC loftee variants."
     echo ""
     echo "Gene filter options:"
     echo "     -L  - list file with the gene names (default: ${geneListFile})."
@@ -93,6 +85,7 @@ It pools results together within one chunk."
     echo "     -w  - working directory (default: current working directory)"
     echo "     -b  - Keep temporary files."
     echo "     -a  - Skip testing variants."
+    echo "     -i  - p-value threshold (default: 1e-5)"
     echo ""
     echo "Monster parameters:"
     echo "     -p  - phenotype (required, no default)"
@@ -112,7 +105,7 @@ function failed (){
     message="${1}"
 
     echo "[Error] Gene has failed: ${gene}"
-    ( >&2 echo "[Error] Gene has failed: ${gene}" ) # Reporting to the error log as well.
+    echo "[Error] Gene has failed: ${gene}" >&2 # Reporting to the error log as well.
     echo "[Error] ${message}"
     echo "[Error] folder moved to ${folder}/failed"
     echo ""
@@ -208,12 +201,12 @@ function testhit (){
 
 # --- Capture command line options --------------------------------------------
 
-# If help message is needed:
+# If help message is printed if no command line arguments has been passed:
 if [ $# == 0 ]; then display_help; fi
 
 # Looping through all command line options:
 OPTIND=1
-while getopts ":hg:m:w:c:l:e:p:s:x:d:ak:t:oL:bf" optname; do
+while getopts ":hg:m:w:c:l:e:p:s:x:d:ak:t:oL:bfi:" optname; do
     case "$optname" in
       # gene filter parameters:
         "L") geneListFile=${OPTARG} ;;
@@ -225,6 +218,7 @@ while getopts ":hg:m:w:c:l:e:p:s:x:d:ak:t:oL:bf" optname; do
 
       # Wrapper option:
         "b") keep_temp=1;;
+        "i") signifThreshold=${OPTARG};;
 
       # variant filter parameters:
         "g") gencode=${OPTARG} ;;
@@ -263,6 +257,7 @@ echo -e "\tPhenotype folder: ${phenotypeDir}"
 echo -e "\tScript dir: ${scriptDir}"
 echo -e "\tKinship matrix: ${kinshipMatrix}"
 echo -e "\tSingle point association results: ${singlePointDir}"
+echo -e "\tMONSETER executable: ${MONSTER}"
 echo ""
 
 
@@ -270,14 +265,15 @@ echo ""
 folder=""
 commandOptions=""
 
-# Exiting if the gene list could not opened:
+# Exiting if the gene list does not exist:
 if [[ ! -e $geneListFile ]]; then
-    echo "[Error] $geneListFile could not opened. Exiting."
+    echo "[Error] $geneListFile does not exist. Exiting."
     exit;
 fi
 
 echo "[Info] The following parameters were submitted:"
-# 1. GENCODE -expecting a list of feature names separated by a comma.
+
+# GENCODE -expecting a list of feature names separated by a comma.
 if [ ! -z $gencode ]; then
     folder=${gencode}
     commandOptions=" -G ${gencode}"
@@ -290,44 +286,45 @@ if [ -z $rootDir ]; then
     echo -e "\tWorking directory: $rootDir"
 fi
 
-# Reporting if temporary files are saved:
+# Reporting if temporary files should be saved:
 if [[ ${keep_temp} -eq 1 ]]; then echo -e "\tTemporary files will not be deleted!"; fi
 
-# 2. GTEx - expecting a list of feature names separeted by comma.
+# GTEx - expecting a list of feature names separeted by comma.
 if [ ! -z $gtex ]; then
     folder=${folder}".GTEX."${gtex}
     commandOptions=${commandOptions}" -E ${gtex}"
     echo -e "\tGTEx feaures: ${gtex}"
 fi
 
-# 3. Overlap - expecting a list of features separeated by comma.
+# Overlap - expecting a list of features separeated by comma.
 if [ ! -z $overlap ]; then
     folder=${folder}".Overlap."${overlap}
     commandOptions=${commandOptions}" -L ${overlap}"
     echo -e "\tOverlapping reg.feaures: ${overlap}"
 fi
 
-# 4. MAF - expecting a float between 0 and 0.5
+# MAF - expecting a float between 0 and 0.5
 if [ ! -z $MAF ]; then
     folder=${folder}".MAF."${MAF}
     commandOptions=${commandOptions}" --maf ${MAF}"
     echo -e "\tUpper minor allele frequency: ${MAF}"
 fi
 
-# 5. If lof is set, we only need loss of function variants:
+# IF loftee is set, only those variants are included in the test that were replrted to be LC or HC lof variants by loftee:
 if [ ! -z $loftee ]; then
     folder=${folder}".loftee"
     commandOptions=${commandOptions}" --loftee "
     echo -e "\tOnly loss-of-function variants will be considered (loftee HC and LC)."
 fi
-## lines for severe variants:
+
+# If lof is set, only variants with severe consequences will be selected.
 if [ ! -z $lof ]; then
     folder=${folder}".lof"
     commandOptions=${commandOptions}" --lof "
     echo -e "\tOnly variants with severe consequences will be considered (down to missense)."
 fi
 
-# 5. Score - If score is not given we apply no score. Otherwise we test the submitted value:
+# Score - If score is not given we apply no score. Otherwise we test the submitted value:
 # Accepted scores:
 if [[ ! -z ${score} ]]; then
     score=${score^^}
@@ -358,28 +355,30 @@ if [ ${score} == "Eigen" ]; then commandOptions=${commandOptions}" --shift 1 "; 
 # Adjust dir name:
 folder=${folder}"."${score}
 
-# 6. Extend regions:
+# Exons might be etended with a given number of residues:
 if [ ! -z $xtend ]; then
     folder=${folder}".Xtend."${xtend}
     commandOptions=${commandOptions}" --extend ${xtend}"
     echo -e "\tFeatures are extended by ${xtend}bp"
 fi
 
-# 7. Set score cutoff:
+# Setting score cutoff, below which the variant will be removed from the test:
 if [ ! -z ${cutoff} ]; then
     folder=${folder}".cutoff."${cutoff}
     echo -e "\tScore cutoff: ${cutoff}"
     commandOptions=${commandOptions}" --cutoff ${cutoff}"
 fi
+
+# Setting score shift, a number that will be added to every scores (MONSTER does not accept scores <= 0!!):
 if [ ! -z ${scoreshift} ]; then
     folder=${folder}".shift."${scoreshift}
     echo -e "\tScores shifted by: ${scoreshift}"
     commandOptions=${commandOptions}" --shift ${scoreshift}"
 fi
 
-# 8. If phenotype is not given the script dies:
+# Checking if phenotype is provided and if that phenotype is proper:
 if [ -z $phenotype ]; then
-    echo "[Error] Phenotype was not set! Exiting"
+    echo "[Error] Phenotype was not set! Exiting." # If phenotype is not given the script dies:
     exit 1
 elif [ ! -e "${phenotypeDir}/MANOLIS.${phenotype}.txt" ]; then
     echo "[Error] Phenotype file was not found! ${phenotypeDir}/MANOLIS.${phenotype}.txt"
@@ -388,20 +387,15 @@ fi
 echo -e "\tPhenotype: ${phenotype}"
 echo -e "\tPhenotype file: ${phenotypeDir}/MANOLIS.${phenotype}.txt"
 
-# Chunk number can be explicitly submitted, or read from the LSB_JOBINDEX
-#if [[ ${LSB_JOBINDEX} -ne 0 ]]; then chunkNo=${LSB_JOBINDEX}; fi;
+# Based on the size of the list and the chunk count we determine the size of each chunk:
 chunkSize=$(wc -l ${geneListFile} | perl -lane 'print int($F[0]/$ENV{"chunkCount"}+0.5)')
 
-# Reporting chunk size and count
+# Reporting chunk size and chunk count:
 echo -e "\n[Info] Gene slice report:"
 echo -e "\tNumber of chunks the gene list is split into: ${chunkCount}"
 echo -e "\tCurrent chunk: ${chunkNo}"
 echo -e "\tNumber of genes in one chunk: ${chunkSize}\n"
-
-# Updating working dir, and create folder:
-folder=$( echo $folder | perl -lane '$_ =~ s/^\.//; print $_')
-workingDir=${rootDir}/${folder}/Pheno.${phenotype}
-mkdir -p ${rootDir}
+echo -e "\tSignificance threshold: ${signifThreshold}\n"
 
 # printing out report:
 echo -e "[Info] output folder: ${workingDir}"
@@ -412,6 +406,11 @@ if [[ ! -e ${kinshipMatrix} ]]; then
     echo "[Error] Kinship could not be opened! ${kinshipMatrix}";
     exit 1;
 fi
+
+# Updating working dir, and create folder:
+folder=$( echo $folder | perl -lane '$_ =~ s/^\.//; print $_')
+workingDir=${rootDir}/${folder}/Pheno.${phenotype}
+mkdir -p ${rootDir}
 
 # Do we want to perform test for all variations:
 if [ -z $skiptest ]; then
@@ -513,20 +512,20 @@ ${imputation_method}"
         -g genotype.mod.filtered.txt \
         -s snpfile.mod.txt \
         ${imputation_method}
+    
+    # Checking if output was generated:
+    if [[ ! -e MONSTER.out ]]; then
+        failed "MONSTER has failed for ${gene}, no output has been generated. Temporary files are kept for troubleshooting."
+        continue;
+    fi
 
     # Once MONSTER run is complete, we have to rename output files:
     mv MONSTER.out MONSTER_out_${gene}.out
     mv MONSTER.param MONSTER_out_${gene}.param
 
-    # Checking if output was generated:
-    if [[ ! -e MONSTER_out_${gene}.out ]]; then
-        failed "MONSTER has failed for ${gene}, no output has been generated. Temporary files are kept for troubleshooting."
-        continue;
-    fi
-
-    # Checking if al the p value is also in the file:
+    # Checking if the p value is also in the file:
     if [[ $(tail -1 MONSTER_out_${gene}.out | awk '{print NF}') -ne 5 ]]; then
-        failed "MONSTER has failed for ${gene}, p-value calculated!. Temporary files are kept for troubleshooting."
+        failed "MONSTER has failed for ${gene}, p-value is missing!. Temporary files are kept for troubleshooting."
         continue;
     fi
 
@@ -535,10 +534,10 @@ ${imputation_method}"
     pval=$(tail -1 MONSTER_out_${gene}.out | cut -f5)
 
     # Adding p-values to file:
-    echo -e "${gene}\t${pval:--}\t${varcnt:--}" >> ${workingDir}/gene_set.${chunkNo}/results
+    echo -e "${gene}\t${pval:-NA}\t${varcnt:-NA}" >> ${workingDir}/gene_set.${chunkNo}/results
 
     # If p-value is really low save into a folder:
-    if [[ $(echo $pval | perl -lane 'print int(-log(abs($F[0]))/log(10))') -ge 5 ]]; then
+    if [[ $(echo $pval | perl -lane 'print $F[0] < $ENV{"signifThreshold"} ? 1 : 0') == 1 ]]; then
         echo "[Info] Found a hit! ${gene} p-value: ${pval}";
         if [ -z $skiptest ]; then
             echo "[Info] Testing if a single variant dirves the associations..."
@@ -547,13 +546,12 @@ ${imputation_method}"
         savehit; # Saving all intermediate files.
     fi
 
+    cd ${workingDir};
     # Cleaning up: removing all files or just the mod files if requested.
-    if [[ ${keep_temp} -eq 1 ]]; then
-        echo "cicaful"
-        # rm ${workingDir}/gene_set.${chunkNo}/${gene}/*mod*;
-    else
-        cd ${workingDir};
+    if [[ ${keep_temp} -ne 1 ]]; then
         rm -rf ${workingDir}/gene_set.${chunkNo}/${gene};
+    else
+        rm ${workingDir}/gene_set.${chunkNo}/${gene}/*mod*
     fi
 
 done
