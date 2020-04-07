d121ab774c1ca0df17de2e64eb7fcf7228183bf2
andrei-barysenka <andrei.barysenka@helmholtz-muenchen.de>
2020-04-07 18:21:32 +0000

    Fix a bug in get_exon_GTEx_overlap.sh

--- testing/get_exon_GTEx_overlap.sh
+++ testing/get_exon_GTEx_overlap.sh
@@ -27,7 +27,7 @@ zcat $gencode|while read chr start end gname ID;do
     tabix $ilist $tabixstr > 03.variants.exon.GTEx.overlap.txt
 
     # remove indels, output 0-based bed
-    cat 03.variants.exon.GTEx.overlap.txt| perl -lne '@a=split(/\t/);$,="\t";if (length($a[3])==1 && length($a[4])==1){print $a[0],$a[1]-1,$a[1],$a[0]."_".$a[1]."_".$a[3]."_".$a[4];}' > 04.variants.exon.GTEx.overlap.noindels.bed
+    cat 03.variants.exon.GTEx.overlap.txt| perl -lne '@a=split(/\t/);$,="\t";if (length($a[3])==1 && length($a[4])==1){print "chr".$a[0],$a[1]-1,$a[1],$a[0]."_".$a[1]."_".$a[3]."_".$a[4];}' > 04.variants.exon.GTEx.overlap.noindels.bed
     
     # liftOver
     liftOver 04.variants.exon.GTEx.overlap.noindels.bed $chainfile 05.liftover.out.exon.GTEx.overlap.bed 05.unmapped.exon.GTEx.overlap.bed
