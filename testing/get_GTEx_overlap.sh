1051aa82db0703b81ccda613f0ac85bf8a78bbe7
andrei-barysenka <andrei.barysenka@helmholtz-muenchen.de>
2020-04-08 12:08:31 +0000

    Add verbosity to get_GTEx_overlap.sh

--- testing/get_GTEx_overlap.sh
+++ testing/get_GTEx_overlap.sh
@@ -8,7 +8,7 @@ outfile=$5
 
 chainfile="/usr/local/bin/burden_testing/hg38ToHg19.over.chain"
 
-# Equivalent of: -e promoter,enhancer,TF_binding_site -l promoter,enhancer,TF_binding_site -s EigenPhred
+# Equivalent of: -e promoter,enhancer,TF_bind -l promoter,enhancer,TF_bind -s EigenPhred
 
 zcat $gencode|while read chr start end gname ID;do
     echo "tabix $lfeatures $chr:$start-$end"
@@ -32,9 +32,8 @@ zcat $gencode|while read chr start end gname ID;do
     n=$(cat 05.liftover.out.GTEx.overlap.bed | wc -l)
 
     if [[ $n -ne 0 ]];then
-	echo "tabix $eigenfile $c":"$e"-"$e";
-	cat 05.liftover.out.GTEx.overlap.bed | while read c s e d;do c=${c/#chr};read -r x z r a <<<$(echo $d| tr '_' ' '); lines=$(tabix $eigenfile $c":"$e"-"$e);score=$(echo "$lines" | awk -v r=$r -v a=$a 'BEGIN{FS="\t";b="NA";}($3==r && $4==a) || ($3==a && $4==r){b=$7;}END{print b;}');if [[ -z $score ]];then score="NA";fi;echo $ID $x $z "." $r $a $score;done | tr ' ' '\t' >> $outfile
-
+	cat 05.liftover.out.GTEx.overlap.bed | while read c s e d;do c=${c/#chr};read -r x z r a <<<$(echo $d| tr '_' ' '); echo "tabix $eigenfile $c":"$e"-"$e";lines=$(tabix $eigenfile $c":"$e"-"$e);score=$(echo "$lines" | awk -v r=$r -v a=$a 'BEGIN{FS="\t";b="NA";}($3==r && $4==a) || ($3==a && $4==r){b=$7;}END{print b;}');if [[ -z $score ]];then score="NA";fi;echo $ID $x $z "." $r $a $score | tr ' ' '\t' >> $outfile;done 
+	$c":"$e"-"$e"
 	echo $ID
     fi
     
