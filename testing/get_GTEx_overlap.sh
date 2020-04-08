2f3e9a45e0c0cbd9672cf669502af44c259f6843
andrei-barysenka <andrei.barysenka@helmholtz-muenchen.de>
2020-04-08 11:02:38 +0000

    Add verbosity to get_GTEx_overlap.sh

--- testing/get_GTEx_overlap.sh
+++ testing/get_GTEx_overlap.sh
@@ -11,6 +11,7 @@ chainfile="/usr/local/bin/burden_testing/hg38ToHg19.over.chain"
 # Equivalent of: -e promoter,enhancer,TF_binding_site -l promoter,enhancer,TF_binding_site -s EigenPhred
 
 zcat $gencode|while read chr start end gname ID;do
+    echo "tabix $lfeatures $chr:$start-$end"
     tabix $lfeatures $chr:$start-$end | grep "\"gene_ID\":\"$ID\""| cut -f 5| perl -MJSON -lne '%h = %{decode_json($_)};if ( ($h{source} eq "GTEx" && ( $h{class} eq "enhancer" || $h{class} eq "promoter" || $h{class} eq "TF_binding_site")) || ($h{source} eq "overlap" && ( $h{class} eq "enhancer" || $h{class} eq "promoter" || $h{class} eq "TF_binding_site")) ){$,="\t";print $h{"chr"},$h{"start"},$h{"end"};}' | sort -k1,1 -k2,2n > 01.regions.GTEx.overlap.bed
 
     mergeBed -i 01.regions.GTEx.overlap.bed > 02.regions.merged.GTEx.overlap.bed
@@ -31,7 +32,7 @@ zcat $gencode|while read chr start end gname ID;do
     n=$(cat 05.liftover.out.GTEx.overlap.bed | wc -l)
 
     if [[ $n -ne 0 ]];then
-	cat 05.liftover.out.GTEx.overlap.bed | while read c s e d;do c=${c/#chr};read -r x z r a <<<$(echo $d| tr '_' ' '); lines=$(tabix $eigenfile $c":"$e"-"$e);score=$(echo "$lines" | awk -v r=$r -v a=$a 'BEGIN{FS="\t";b="NA";}($3==r && $4==a) || ($3==a && $4==r){b=$7;}END{print $b;}');if [[ -z $score ]];then score="NA";fi;echo $ID $x $z "." $r $a $score;done | tr ' ' '\t' >> $outfile
+	cat 05.liftover.out.GTEx.overlap.bed | while read c s e d;do c=${c/#chr};read -r x z r a <<<$(echo $d| tr '_' ' ');echo "tabix $eigenfile $c":"$e"-"$e"; lines=$(tabix $eigenfile $c":"$e"-"$e);score=$(echo "$lines" | awk -v r=$r -v a=$a 'BEGIN{FS="\t";b="NA";}($3==r && $4==a) || ($3==a && $4==r){b=$7;}END{print $b;}');if [[ -z $score ]];then score="NA";fi;echo $ID $x $z "." $r $a $score;done | tr ' ' '\t' >> $outfile
 
 	echo $ID
     fi
