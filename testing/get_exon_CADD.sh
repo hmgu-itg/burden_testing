ac36bacc4710b0c4963bc9a9ba2684f5bd05a1be
andrei-barysenka <andrei.barysenka@helmholtz-muenchen.de>
2020-04-08 10:10:14 +0000

    Fix a bug in testing scripts

--- testing/get_exon_CADD.sh
+++ testing/get_exon_CADD.sh
@@ -13,10 +13,8 @@ fi
 export extend=$extend
 
 zcat $gencode|while read chr start end gname ID;do
-#    echo "Tabix linked features"
     tabix $lfeatures $chr:$start-$end | grep "\"gene_ID\":\"$ID\""| cut -f 5| perl -MJSON -lne 'BEGIN{$extend=$ENV{extend};}{%h = %{decode_json($_)};if ($h{source} eq "GENCODE" && $h{class} eq "exon"){$,="\t";print $h{"chr"},$h{"start"}-$extend,$h{"end"}+$extend;}}' | sort -k1,1 -k2,2n > 01.regions.CADD.bed
 
-#    echo "mergeBed"
     mergeBed -i 01.regions.CADD.bed > 02.regions.merged.CADD.bed
 
     # selecting variants from ilist
@@ -24,9 +22,8 @@ zcat $gencode|while read chr start end gname ID;do
     tabixstr=$(cat 02.regions.merged.CADD.bed| perl -lne '@a=split(/\t/);$s=$a[1]+1;print $a[0].":".$s."-".$a[2];'| tr '\n' ' ')
     tabix $ilist $tabixstr > 03.variants.CADD.txt
 
-    # get CADD scores, omitting indels#
-#    echo "CADD scores"
-    cat 03.variants.CADD.txt | perl -lne '@a=split(/\t/);$,="\t";if (length($a[3])==1 && length($a[4])==1){print $a[0],$a[1],$a[3],$a[4];}'|while read c p r a;do score=$(tabix $caddfile $c":"$p"-"$p| awk -v a=$a 'BEGIN{FS="\t";}$4==a{print $6;}');echo $ID $c $p "." $r $a $score;done | tr ' ' '\t' >> $outfile
+    # get CADD scores, omitting indels
+    cat 03.variants.CADD.txt | perl -lne '@a=split(/\t/);$,="\t";if (length($a[3])==1 && length($a[4])==1){print $a[0],$a[1],$a[3],$a[4];}'|while read c p r a;do lines=$(tabix $caddfile $c":"$p"-"$p);score=$(echo "$lines" | awk -v a=$a 'BEGIN{FS="\t";}$4==a{print $6;}');if [[ -z $score ]];then score="NA";fi;echo $ID $c $p "." $r $a $score;done | tr ' ' '\t' >> $outfile
     echo $ID
 done
 
