cac05d5403837733dfafd7c2e0705c2ce9509233
andrei <andrei.barysenka@helmholtz-muenchen.de>
2020-11-27 17:24:40 +0100

    Add config.py

---
+++ python/config.py
@@ -0,0 +1,31 @@
+
+
+LOF_SEVERITY={
+    "transcript_ablation"      : 10,
+    "splice_acceptor_variant"  : 9,
+    "splice_donor_variant"     : 8,
+    "stop_gained"              : 7,
+    "frameshift_variant"       : 6,
+    "stop_lost"                : 5,
+    "start_lost"               : 4,
+    "transcript_amplification" : 3,
+    "inframe_insertion"        : 2,
+    "inframe_deletion"         : 1
+}
+
+EIGEN_SPECS={
+    "CHR"=1,
+    "POS"=2,
+    "REF"=3,
+    "ALT"=4,
+    "SCORE"=7
+}
+
+CADD_SPECS={
+    "CHR"=1,
+    "POS"=2,
+    "REF"=3,
+    "ALT"=4,
+    "SCORE"=6
+}
+
